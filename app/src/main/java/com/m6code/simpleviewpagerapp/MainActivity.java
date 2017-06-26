35bf4c9a104002944e83df36debd1a108373a765
Benjamin M6 <benjamynfrank101@gmail.com>
2017-06-26 19:14:55 +0100

    Set-Up the MainActivity.java to use the ViewPager and TabLayout to Load each fragment

--- app/src/main/java/com/m6code/simpleviewpagerapp/MainActivity.java
+++ app/src/main/java/com/m6code/simpleviewpagerapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.m6code.simpleviewpagerapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        SimpleFragmentPagerAdapter adapter = new SimpleFragmentPagerAdapter(this, getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        // Set the tab
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
